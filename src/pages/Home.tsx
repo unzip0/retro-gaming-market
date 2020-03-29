ba5a1575ec9782c6aada042cebfb4e73800d2247
tandem-dev <adrian@tandemprojects.cat>
2020-03-29 18:13:59 +0200

    android config

---
+++ src/pages/Home.tsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react';
+import { IonPage, IonHeader, IonContent, IonToolbar, IonTitle, IonButton } from "@ionic/react"
+
+
+
+
+const Home: React.FC = () => {
+    const [input, setInput] = useState<string>('')
+
+    useEffect(() => {
+        console.log(input)
+    }, [input])
+
+    return (
+        <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>HOME PAGE</IonTitle>
+                </IonToolbar>
+                <IonContent className="ion-padding">
+                    <IonButton routerLink="/login">Login</IonButton>
+                </IonContent>
+            </IonHeader>
+        </IonPage>
+    )
+}
+
+export default Home
