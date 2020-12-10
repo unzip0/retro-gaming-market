9968cb32af57bb1f6d3ad105c8c856aab0111c2b
unzip0 <apcabrera08@gmail.com>
2020-12-10 17:49:00 +0100

    not found image and path

--- src/config/url.config.js
+++ src/config/url.config.js
@@ -1,5 +1,6 @@
 module.exports = {
     LOGIN: 'http://localhost:3000/api/users/login',
     PRODUCT_FILTER: 'http://localhost:3000/api/products/filter',
-    IMAGES: 'https://www.retrogamingmarket.es/images/'
+    IMAGES: 'https://www.retrogamingmarket.es/images/',
+    IMAGE_NOT_FOUND: '/assets/images/art-not-found.jpg'
 };
