3c7727e17fcf82b58be2eecc5f23241af887bee1
unzip0 <apcabrera08@gmail.com>
2020-12-13 20:42:31 +0100

    toggle menu -> logout dont work

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import { IonContent, IonMenu, IonHeader, IonToolbar, IonTitle, IonList, IonItem, IonRouterOutlet, IonIcon, IonText } from "@ionic/react";
+import { menuController } from "@ionic/core";
 
 const Menu: React.FC = () => {
+
+    const toggleMenu = async() => {
+        await menuController.toggle();
+    }
 
     return (
         <IonMenu side="start" menuId="menu" type="push" contentId="menu">
@@ -12,24 +17,24 @@ const Menu: React.FC = () => {
             </IonHeader>
             <IonContent color="dark">
                 <IonList class="ion-no-padding">
-                    <IonItem routerLink="/home" color="dark">
+                    <IonItem routerLink="/home" color="dark" onClick={toggleMenu}>
                         <IonIcon src="assets/icon/home-icon.svg"/> 
                         <IonText>Inicio</IonText>
                     </IonItem>
-                    <IonItem routerLink="/profile" color="dark">
+                    <IonItem routerLink="/profile" color="dark" onClick={toggleMenu}>
                         <IonIcon src="assets/icon/user-icon.svg"/> 
                         <IonText>Perfil</IonText>
                     </IonItem>
-                    <IonItem routerLink="/product" color="dark">
+                    <IonItem routerLink="/product" color="dark" onClick={toggleMenu}>
                         <IonIcon src="assets/icon/add-game-icon.svg"/> 
                         <IonText>Vender producto</IonText>
                     </IonItem>
                     <IonItem color="dark">
-                        <IonIcon src="assets/icon/list-icon.svg"/> 
+                        <IonIcon src="assets/icon/list-icon.svg" onClick={toggleMenu}/> 
                         <IonText>Tus productos</IonText>
                     </IonItem>
                     <IonItem routerLink="/" color="dark">
-                        <IonIcon src="assets/icon/logout-icon.svg"/> 
+                        <IonIcon src="assets/icon/logout-icon.svg" onClick={toggleMenu}/> 
                         <IonText>Cerrar sesión</IonText>
                     </IonItem>
                 </IonList>
